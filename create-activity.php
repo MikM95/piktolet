bac07da54c9dc9aac12dfb518c7f21f7c5e7f004
MikM95 <mikmejer95@gmail.com>
2021-10-24 18:38:05 +0200

    fixes

--- create-activity.php
+++ create-activity.php
@@ -7,37 +7,34 @@ include('templates/header.php');
        <form method="POST" enctype="multipart/form-data">
         <div class="form-row align-items-center">
           <div class="col-sm-3 my-1">
-           <label for="name">Navn på Aktivitet</label>
-           <textarea class="form-control" id="name" rows="1"></textarea>
+           <label >Navn på Aktivitet</label>
+           <textarea name="name" for="name" class="form-control" id="name" rows="1"></textarea>
          </div>
          <div class="col-sm-3 my-1">
-           <label for="time">Vælg venligst tidspunkt for aktiviteten</label>
-           <input type="datetime-local" name="time">
+           <label >Vælg venligst tidspunkt for aktiviteten</label>
+           <input name="time" for="time" type="datetime-local">
          </div>
          <div class="col-sm-3 my-1">
-           <label for="description">Beskrivelse</label>
-           <textarea class="form-control" id="description" rows="3"></textarea>
+           <label >Beskrivelse</label>
+           <textarea name="description" for="description" class="form-control" id="description" rows="3"></textarea>
          </div>
-        </div>
-        <div class="col-sm-3 my-1">
-          <input type="file" name="picture"/>
+         <div class="col-sm-3 my-1">
+           <label >Web link til billedet</label>
+           <textarea name="picture" for="picture" class="form-control" id="picture" rows="3"></textarea>
+         </div>
         </div>
         <button type="submit" class="btn btn-primary">Opret</button>
       </form>
 
       <?php
-      if (isset($_POST['name'], $_POST['time'], $_POST['description'], $_POST['picture'],))
+      if (isset($_POST['name'], $_POST['time'], $_POST['description'], $_POST['picture']))
           {
+            echo "kage";
             $name = $_POST['name'];
             $time = $_POST['time'];
             $description = $_POST['description'];
             $picture = $_POST['picture'];
 
-
-$db_data= performQuery("SELECT * FROM events");
-        while ($row = mysqli_fetch_assoc($db_data)) {
-          $id = $row['id'];
-          performQuery("INSERT INTO events('name', 'time', 'description', 'picture') VALUES ('$name', '$time', '$description', '$picture')");
-}
-}
+            performQuery("INSERT INTO events(name, time, description, picture) VALUES ('$name', '$time', '$description', '$picture')");
+          }
        ?>
